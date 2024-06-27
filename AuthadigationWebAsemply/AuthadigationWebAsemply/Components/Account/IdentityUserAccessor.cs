fb2b87549db2455017e354631080fd37157b30e8
Pantelis <pantelkan16@gmail.com>
2024-06-27 13:15:45 +0300

    Added the project and the git ignore files.

---
+++ AuthadigationWebAsemply/AuthadigationWebAsemply/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using AuthadigationWebAsemply.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthadigationWebAsemply.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
